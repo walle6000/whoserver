405feda1b91f924edc5d8dcf1adc459a743fa285
huang yi gang <huangyg2@zuk.com>
2017-07-07 12:58:25 +0800

    add data source pool and junit test

--- src/test/java/io/swagger/daoTest/UserDaoTest.java
+++ src/test/java/io/swagger/daoTest/UserDaoTest.java
@@ -1,23 +1,19 @@
 package io.swagger.daoTest;
 
-import static org.junit.Assert.*;
-
-import javax.sql.DataSource;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import io.swagger.Swagger2SpringBoot;
-import io.swagger.api.TopicApi;
 import io.swagger.dao.UserDao;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-classes=Swagger2SpringBoot.class)
+@SpringBootTest(classes=Swagger2SpringBoot.class)
 @WebAppConfiguration
 public class UserDaoTest {
 
