d98c58727dce7c5c7f5fe7d46cca7bba4a7f1846
huang yi gang <huangyg2@zuk.com>
2017-07-10 17:55:36 +0800

    Add verify code

---
+++ src/main/java/io/swagger/servlet/VerifyCodeServlet.java
@@ -0,0 +1,47 @@
+package io.swagger.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.swagger.utils.VerifyCodeUtils;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns="/verifyCode/get", description="获取验证码图片")
+public class VerifyCodeServlet extends HttpServlet {
+	
+	private Logger logger = LoggerFactory.getLogger(VerifyCodeServlet.class);
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		logger.info("VerifyCodeServlet - doGet start to get VerifyCode img");
+		resp.setHeader("Pragma", "No-cache"); 
+		resp.setHeader("Cache-Control", "no-cache"); 
+		resp.setDateHeader("Expires", 0); 
+		resp.setContentType("image/jpeg"); 
+        //生成随机字串 
+        String verifyCode = VerifyCodeUtils.generateVerifyCode(4); 
+        logger.info("VerifyCodeServlet - doGet VerifyCode:" + verifyCode);
+        //存入会话session 
+        HttpSession session = req.getSession(true); 
+        //删除以前的
+        session.removeAttribute("verCode");
+        session.setAttribute("verCode", verifyCode.toLowerCase()); 
+        //生成图片 
+        int w = 100, h = 30; 
+        VerifyCodeUtils.outputImage(w, h, resp.getOutputStream(), verifyCode);  
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	logger.info("VerifyCodeServlet - doPost start to get VerifyCode img");
+    	doGet(req,resp);
+    }
+}
