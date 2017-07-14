4bde92feba624df51db406980f0b5d30479fd037
huang yi gang <huangyg2@zuk.com>
2017-07-14 17:18:04 +0800

    Add file upload

---
+++ src/main/java/io/swagger/configuration/FileUploadConfig.java
@@ -0,0 +1,33 @@
+package io.swagger.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class FileUploadConfig {
+
+	@Value("${fileUpload.maxUploadSize}")  
+	private long maxUploadSize;
+	
+	@Value("${fileUpload.maxInMemorySize}")  
+	private int maxInMemorySize;
+	
+	@Value("${fileUpload.maxUploadSizePerFile}")    
+	private long maxUploadSizePerFile;
+	
+	@Value("${fileUpload.defaultEncoding}")    
+	private String defaultEncoding;
+	
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(maxUploadSize); //1Mb
+        multipartResolver.setMaxInMemorySize(maxInMemorySize);
+        multipartResolver.setMaxUploadSizePerFile(maxUploadSizePerFile);
+        multipartResolver.setDefaultEncoding(defaultEncoding);
+        return multipartResolver;
+    }
+}
